249bb9851dbb1cc197abcf69acfe98e4d68bfbfe
1183029827 <74184710+1183029827@users.noreply.github.com>
2024-05-16 08:42:27 +0800

    Add files via upload

---
+++ TCGUI.py
@@ -0,0 +1,22 @@
+import matlab.engine
+import tkinter as tk
+# 启动 MATLAB 引擎
+eng = matlab.engine.start_matlab()
+# 调用MATLAB部分由宋纯豪同学实现
+
+# GUI界面部分由王禹轩 王财成 彭万同学实现
+
+# 创建 GUI 界面（王禹轩）
+# 计算 arcsin 的部分（王禹轩）
+
+
+# 计算 cos 的部分（王财成）
+
+
+# 计算 sin 的部分（王财成）
+
+
+# 计算 arctan 的部分（彭万）
+
+# 关闭 MATLAB 引擎
+eng.quit()
